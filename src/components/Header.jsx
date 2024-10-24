5cd5c7f783d0c5f76ceeca35c6d19667345f75bd
Seyoon Jeong <seyoonagain@gmail.com>
2024-10-24 15:38:30 +0900

    Feat: Apply Tailwind CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
+const LIST_STYLE = 'list-none font-normal text-[13px]';
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className='flex justify-between items-center py-[10px] px-5 bg-black'>
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className='flex justify-center items-center gap-5'>
+        <li className={LIST_STYLE}>로그인</li>
+        <li className={LIST_STYLE}>회원가입</li>
+        <li className={LIST_STYLE}>내클래스</li>
       </ul>
     </header>
   );
